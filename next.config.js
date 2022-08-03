03d214d9841ae1502d414b587bc10d180faf364e
Eric Ahmann <eric@ahmann.me>
2022-08-03 11:15:25 -0500

    Fixing images on pages and local???

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const ghPages = process.env.NEXT_PUBLIC_GH_PAGES ? "/ahmann-dev" : undefined
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    loader: "akamai",
-    path: ""
-  },
 }
 
 module.exports = nextConfig
